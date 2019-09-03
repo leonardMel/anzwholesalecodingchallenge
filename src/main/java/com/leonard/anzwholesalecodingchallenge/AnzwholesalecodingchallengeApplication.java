fe7599b9f23f714cbb7ee42ee2d9eaed14d56f63
BlackSugarr <limeng0525@gmail.com>
2019-09-03 22:43:48 +1000

    initial commit

---
+++ src/main/java/com/leonard/anzwholesalecodingchallenge/AnzwholesalecodingchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.leonard.anzwholesalecodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnzwholesalecodingchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnzwholesalecodingchallengeApplication.class, args);
+	}
+
+}
